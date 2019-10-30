89445e52eca55ee97b8f7cc6096aaa1b2ec5296b
Lizh <4a5g0030@stust.edu.tw>
2019-10-31 02:16:58 +0800

    Modify training
    The reason is that the original can't training 2 class

--- training.py
+++ training.py
@@ -1,6 +1,6 @@
 """
 Mask R-CNN
-Train on the toy bottle dataset and implement color splash effect.
+Train on the toy Balloon dataset and implement color splash effect.
 Copyright (c) 2018 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
@@ -8,15 +8,15 @@ Written by Waleed Abdulla
 Usage: import the module (see Jupyter notebooks for examples), or run from
        the command line as such:
     # Train a new model starting from pre-trained COCO weights
-    python3 bottle.py train --dataset=/home/datascience/Workspace/maskRcnn/Mask_RCNN-master/samples/bottle/dataset --weights=coco
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=coco
     # Resume training a model that you had trained earlier
-    python3 bottle.py train --dataset=/path/to/bottle/dataset --weights=last
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=last
     # Train a new model starting from ImageNet weights
-    python3 bottle.py train --dataset=/path/to/bottle/dataset --weights=imagenet
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=imagenet
     # Apply color splash to an image
-    python3 bottle.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
+    python3 balloon.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
     # Apply color splash to video using the last weights you trained
-    python3 bottle.py splash --weights=last --video=<URL or path to file>
+    python3 balloon.py splash --weights=last --video=<URL or path to file>
 """
 
 import os
@@ -25,9 +25,6 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
-import cv2
-from mrcnn.visualize import display_instances
-import matplotlib.pyplot as plt
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("./")
@@ -49,7 +46,7 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 ############################################################
 
 
-class CustomConfig(Config):
+class SomethingConfig(Config):
     """Configuration for training on the toy  dataset.
     Derives from the base Config class and overrides some values.
     """
@@ -61,7 +58,7 @@ class CustomConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 2  # Background + dog + cat
+    NUM_CLASSES = 1 + 2  # Background + balloon
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
@@ -74,10 +71,10 @@ class CustomConfig(Config):
 #  Dataset
 ############################################################
 
-class CustomDataset(utils.Dataset):
+class SomethingDataset(utils.Dataset):
 
-    def load_custom(self, dataset_dir, subset):
-        """Load a subset of the bottle dataset.
+    def load_VIA(self, dataset_dir, subset):
+        """Load a subset of the Balloon dataset.
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
@@ -90,7 +87,7 @@ class CustomDataset(utils.Dataset):
         dataset_dir = os.path.join(dataset_dir, subset)
 
         # Load annotations
-        # VGG Image Annotator saves each image in the form:
+        # VGG Image Annotator (up to version 1.6) saves each image in the form:
         # { 'filename': '28503151_5b5b7ec140_b.jpg',
         #   'regions': {
         #       '0': {
@@ -104,9 +101,9 @@ class CustomDataset(utils.Dataset):
         #   'size': 100202
         # }
         # We mostly care about the x and y coordinates of each region
-        annotations1 = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        # print(annotations1)
-        annotations = list(annotations1.values())  # don't need the dict keys
+        # Note: In VIA 2.0, regions was changed from a dict to a list.
+        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        annotations = list(annotations.values())  # don't need the dict keys
 
         # The VIA tool saves images in the JSON even if they don't have any
         # annotations. Skip unannotated images.
@@ -114,11 +111,13 @@ class CustomDataset(utils.Dataset):
 
         # Add images
         for a in annotations:
-            # print(a)
             # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. There are stores in the
+            # the outline of each object instance. These are stores in the
             # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+
             polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            names = [r['region_attributes'] for r in a['regions'].values()]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
@@ -128,11 +127,12 @@ class CustomDataset(utils.Dataset):
             height, width = image.shape[:2]
 
             self.add_image(
-                "animal",  ## for a single class just add the name here
+                "animal",
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
-                polygons=polygons)
+                polygons=polygons,
+                names=names)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -141,7 +141,7 @@ class CustomDataset(utils.Dataset):
             one mask per instance.
         class_ids: a 1D array of class IDs of the instance masks.
         """
-        # If not a bottle dataset image, delegate to parent class.
+        # If not a balloon dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
         if image_info["source"] != "animal":
             return super(self.__class__, self).load_mask(image_id)
@@ -149,6 +149,7 @@ class CustomDataset(utils.Dataset):
         # Convert polygons to a bitmap mask of shape
         # [height, width, instance_count]
         info = self.image_info[image_id]
+        class_names = info["names"]
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
                         dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
@@ -156,9 +157,20 @@ class CustomDataset(utils.Dataset):
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
+        # Assign class_ids by reading class_names
+        class_ids = np.zeros([len(info["polygons"])])
+        # In the surgery dataset, pictures are labeled with name 'a' and 'r' representing arm and ring.
+        for i, p in enumerate(class_names):
+        #"name" is the attributes name decided when labeling, etc. 'region_attributes': {name:'a'}
+            if p['name'] == 'dog':
+                class_ids[i] = 1
+            elif p['name'] == 'cat':
+                class_ids[i] = 2
+                # assert code here to extend to other labels
+        class_ids = class_ids.astype(int)
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+        return mask.astype(np.bool), class_ids
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -172,13 +184,13 @@ class CustomDataset(utils.Dataset):
 def train(model):
     """Train the model."""
     # Training dataset.
-    dataset_train = CustomDataset()
-    dataset_train.load_custom(args.dataset, "train")
+    dataset_train = BalloonDataset()
+    dataset_train.load_balloon(args.dataset, "train")
     dataset_train.prepare()
 
     # Validation dataset
-    dataset_val = CustomDataset()
-    dataset_val.load_custom(args.dataset, "val")
+    dataset_val = BalloonDataset()
+    dataset_val.load_balloon(args.dataset, "val")
     dataset_val.prepare()
 
     # *** This training schedule is an example. Update to your needs ***
@@ -201,13 +213,13 @@ def color_splash(image, mask):
     # Make a grayscale copy of the image. The grayscale copy still
     # has 3 RGB channels, though.
     gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-    # We're treating all instances as one, so collapse the mask into one layer
-    mask = (np.sum(mask, -1, keepdims=True) >= 1)
     # Copy color pixels from the original color image where mask is set
-    if mask.shape[0] > 0:
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
         splash = np.where(mask, image, gray).astype(np.uint8)
     else:
-        splash = gray
+        splash = gray.astype(np.uint8)
     return splash
 
 
@@ -262,6 +274,7 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
         vwriter.release()
     print("Saved to ", file_name)
 
+
 ############################################################
 #  Training
 ############################################################
@@ -271,13 +284,13 @@ if __name__ == '__main__':
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect custom class.')
+        description='Train Mask R-CNN to detect balloons.')
     parser.add_argument("command",
                         metavar="<command>",
                         help="'train' or 'splash'")
     parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/custom/dataset/",
-                        help='Directory of the custom dataset')
+                        metavar="/path/to/balloon/dataset/",
+                        help='Directory of the Balloon dataset')
     parser.add_argument('--weights', required=True,
                         metavar="/path/to/weights.h5",
                         help="Path to weights .h5 file or 'coco'")
@@ -306,9 +319,9 @@ if __name__ == '__main__':
 
     # Configurations
     if args.command == "train":
-        config = CustomConfig()
+        config = SomethingConfig()
     else:
-        class InferenceConfig(CustomConfig):
+        class InferenceConfig(SomethingConfig):
             # Set batch size to 1 since we'll be running inference on
             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
             GPU_COUNT = 1
@@ -332,7 +345,7 @@ if __name__ == '__main__':
             utils.download_trained_weights(weights_path)
     elif args.weights.lower() == "last":
         # Find last trained weights
-        weights_path = model.find_last()[1]
+        weights_path = model.find_last()
     elif args.weights.lower() == "imagenet":
         # Start from ImageNet trained weights
         weights_path = model.get_imagenet_weights()
